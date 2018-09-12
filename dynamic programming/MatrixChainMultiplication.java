d25d3af0d4805f802dad4fcd957e35774c7015bd
RKKUNDU <kundu.rohit06@gmail.com>
2018-09-12 16:05:03 +0530

    matrix chain multiplication

---
+++ dynamic programming/MatrixChainMultiplication.java
@@ -0,0 +1,47 @@
+import java.util.Scanner;
+
+public class MatrixChainMultiplication
+{
+    public static void main(String a[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int n;
+        System.out.println("Enter value of n");
+        n=sc.nextInt();
+        System.out.println("Enter "+(n+1)+" value of dimension");
+        int dimension[]=new int[n+1];
+        for(int i=0;i<n+1;i++)
+        {
+            dimension[i]=sc.nextInt();
+        }
+        int ans=solve(dimension);
+        System.out.println("Minimum cost for this multiplication is "+ans);
+
+    }
+    static int solve(int dimension[])
+    {
+        int n=dimension.length-1;
+        int arr[][]=new int[n][n];
+        for(int i=0;i<n;i++)
+        {
+            arr[i][i]=0;
+        }
+        for(int i=1;i<n;i++)
+        {
+            for(int j=0;j<n-i;j++)
+            {
+                int l=j+i;
+                arr[j][l]=Integer.MAX_VALUE;
+                for(int k=0;k<l;k++)
+                {
+                    int x=arr[j][k]+arr[k+1][l]+dimension[j]*dimension[k+1]*dimension[l+1];
+                    if(x<arr[j][l])
+                    {
+                        x=arr[j][l];
+                    }
+                }
+            }
+        }
+        return arr[0][n-1];
+    }
+}
